f013d8e3565683539f5866a9326313362018a769
Mohamed Twakl <mohamedtwaklelbazz@gmail.com>
2025-11-25 22:03:09 +0200

    add dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,11 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    kotlin("kapt")
+    id("com.google.dagger.hilt.android")
+    id ("dagger.hilt.android.plugin")
+    kotlin("plugin.serialization").version("1.7.10")
+    id("kotlinx-serialization")
 }
 
 android {
@@ -40,6 +45,56 @@ android {
 }
 
 dependencies {
+
+    // ViewModel
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.3")
+// Compose Navigation
+    implementation("androidx.navigation:navigation-compose:2.9.3")
+// Voyager  Navigator
+    implementation("cafe.adriel.voyager:voyager-navigator:1.1.0-beta02")
+// Ktor Client
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
+    implementation("io.ktor:ktor-client-core:2.3.11")
+    implementation("io.ktor:ktor-client-android:2.3.11")
+    implementation("io.ktor:ktor-client-content-negotiation:2.3.11")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.11")
+
+
+
+    val room_version = "2.8.0"
+
+    implementation("androidx.room:room-runtime:$room_version")
+
+// If this project uses any Kotlin source, use Kotlin Symbol Processing (KSP)
+// See Add the KSP plugin to your project
+    kapt("androidx.room:room-compiler:$room_version")
+
+// If this project only uses Java source, use the Java annotationProcessor
+// No additional plugins are necessary
+    annotationProcessor("androidx.room:room-compiler:$room_version")
+
+// optional - Kotlin Extensions and Coroutines support for Room
+    implementation("androidx.room:room-ktx:$room_version")
+
+
+// Coil -- Online Image Loader
+    implementation("io.coil-kt:coil-compose:2.7.0")
+
+// Retrofit & Converter
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+
+// logging interceptor
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.10.0")
+
+// DI
+    implementation("com.google.dagger:hilt-android:2.50")
+    kapt("com.google.dagger:hilt-android-compiler:2.50")
+    kapt("androidx.hilt:hilt-compiler:1.0.0")
+    implementation("androidx.hilt:hilt-navigation-fragment:1.0.0")
+
+// For Dagger Hilt to work with Jetpack Compose
+    implementation ("androidx.hilt:hilt-navigation-compose:1.2.0")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
